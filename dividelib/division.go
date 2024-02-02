fc327f6f20197ed377bba6332e029061147e94e0
Arthur Silva <arxdsilva@gmail.com>
2024-02-02 02:25:59 -0800

    add docs

--- dividelib/division.go
+++ dividelib/division.go
@@ -1,6 +1,7 @@
 package dividelib
 
 // Divide returns the result of dividing a by b.
+// It is the caller's responsibility to ensure that b is not zero.
 func Divide(a, b, c int) int {
 	return (a + b) / c
 }
